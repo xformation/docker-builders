da6411699929da923698c75080a7527fa1fd77ff
Papu Bhattacharya <papu.bhattacharya@synectiks.com>
2018-04-14 20:52:09 +0530

    changes for docker buildfile

--- jira/launch.sh
+++ jira/launch.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -o errexit
-
+/opt/jira/setup-jira-db.sh
 cat <<END > /opt/jira-home/dbconfig.xml
 <?xml version="1.0" encoding="UTF-8"?>
 <jira-database-config>
@@ -9,10 +9,10 @@ cat <<END > /opt/jira-home/dbconfig.xml
   <database-type>postgres72</database-type>
   <schema-name>public</schema-name>
   <jdbc-datasource>
-    <url>$DB_JDBC_URL</url>
-    <driver-class>$DB_JDBC_DRIVER</driver-class>
-    <username>$DB_USER</username>
-    <password>$DB_PASSWORD</password>
+    <url>${DB_JDBC_URL}</url>
+    <driver-class>org.postgresql.Driver</driver-class>
+    <username>${DB_USER}</username>
+    <password>${DB_PASSWORD}</password>
     <validation-query>select 1</validation-query>
     <min-evictable-idle-time-millis>60000</min-evictable-idle-time-millis>
     <time-between-eviction-runs-millis>300000</time-between-eviction-runs-millis>
@@ -22,6 +22,8 @@ cat <<END > /opt/jira-home/dbconfig.xml
     <pool-max-idle>20</pool-max-idle>
     <pool-remove-abandoned>true</pool-remove-abandoned>
     <pool-remove-abandoned-timeout>300</pool-remove-abandoned-timeout>
+    <pool-test-on-borrow>false</pool-test-on-borrow>
+    <pool-test-while-idle>true</pool-test-while-idle>
   </jdbc-datasource>
 </jira-database-config>
 END
