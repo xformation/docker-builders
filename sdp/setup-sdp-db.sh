e378b340f6cfab6003049719d6a7cbfc765373a2
Papu Bhattacharya <papu.bhattacharya@synectiks.com>
2018-05-16 21:53:09 +0530

    fixed db name

--- sdp/setup-sdp-db.sh
+++ sdp/setup-sdp-db.sh
@@ -4,7 +4,7 @@ PGPASSWORD=postgres psql -h ${DB_HOST} -p 5432 --username postgres <<- EOSQL
    CREATE USER sdp WITH PASSWORD 'sdp';
    CREATE DATABASE sdp WITH ENCODING 'UNICODE' LC_COLLATE 'C' LC_CTYPE 'C' \
        TEMPLATE template0;
-   GRANT ALL PRIVILEGES ON DATABASE spd to sdp;
+   GRANT ALL PRIVILEGES ON DATABASE sdp to sdp;
 EOSQL
 
 echo "******SDP DATABASE CREATED******"
