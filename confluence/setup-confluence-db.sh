33b8c6fe7020e9e4fb75df3bf7510d199926333c
Papu Bhattacharya <papu.bhattacharya@synectiks.com>
2018-05-23 15:53:00 +0530

    added database import scripts

--- confluence/setup-confluence-db.sh
+++ confluence/setup-confluence-db.sh
@@ -7,3 +7,10 @@ PGPASSWORD=postgres psql -h ${DB_HOST} -p 5432 --username postgres <<- EOSQL
    GRANT ALL PRIVILEGES ON DATABASE confluence to confluence;
 EOSQL
 echo "******CONFLUENCE DATABASE CREATED******"
+if [ -r '/data.sql' ]; then
+    echo "**IMPORTING CONFLUENCE DATABASE BACKUP**"
+    SERVER=$!; sleep 2
+    PGPASSWORD=postgres psql -h ${DB_HOST} -p 5432 --username postgres confluence < /data.sql
+    kill $SERVER; wait $SERVER
+    echo "**Confluence DATABASE BACKUP IMPORTED***"
+fi
