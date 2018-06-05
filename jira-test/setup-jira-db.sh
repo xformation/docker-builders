0077fe58db3a89b952aa20d078b967d847c63d77
Papu Bhattacharya <papu.bhattacharya@synectiks.com>
2018-06-05 13:28:53 +0530

    writing docker files for jira-test env

---
+++ jira-test/setup-jira-db.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+echo "******CREATING JIRA TEST DATABASE******"
+PGPASSWORD=postgres psql -h ${DB_HOST} -p 5432 --username postgres <<- EOSQL
+   CREATE USER jira WITH PASSWORD 'jira';
+   CREATE DATABASE jiratest WITH ENCODING 'UNICODE' LC_COLLATE 'C' LC_CTYPE 'C' \
+       TEMPLATE template0;
+   GRANT ALL PRIVILEGES ON DATABASE jira to jiratest;
+EOSQL
+
+echo "******JIRA TEST DATABASE CREATED******"
+#psql -U postgres -tc "SELECT 1 FROM pg_database WHERE datname = 'my_db'" | grep -q 1 || psql -U postgres -c "CREATE DATABASE my_db"
