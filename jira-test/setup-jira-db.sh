c8aa90465223345b9d403b78c0b434d847dff4bf
Papu Bhattacharya <papu.bhattacharya@synectiks.com>
2018-06-05 13:33:09 +0530

    some fixes on database name

--- jira-test/setup-jira-db.sh
+++ jira-test/setup-jira-db.sh
@@ -4,7 +4,7 @@ PGPASSWORD=postgres psql -h ${DB_HOST} -p 5432 --username postgres <<- EOSQL
    CREATE USER jira WITH PASSWORD 'jira';
    CREATE DATABASE jiratest WITH ENCODING 'UNICODE' LC_COLLATE 'C' LC_CTYPE 'C' \
        TEMPLATE template0;
-   GRANT ALL PRIVILEGES ON DATABASE jira to jiratest;
+   GRANT ALL PRIVILEGES ON DATABASE jiratest to jira;
 EOSQL
 
 echo "******JIRA TEST DATABASE CREATED******"
